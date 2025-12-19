ce3fada8793ebfbca82ff9a5b6836a4c490cfbb7
Sofikul <sofikulislam9757@gmail.com>
2025-12-19 10:51:13 +0530

    Attributes are added to the POJO

--- src/main/java/com/emphub/pro/model/Attendance.java
+++ src/main/java/com/emphub/pro/model/Attendance.java
@@ -1,4 +1,75 @@
 package com.emphub.pro.model;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Attendance {
+
+    private int id;
+    private int employeeId;
+    private LocalDate date;
+    private LocalTime checkIn;
+    private LocalTime checkOut;
+    private double totalHours;
+
+    public Attendance() {
+
+    }
+
+    public Attendance(int id, int employeeId, LocalDate date, LocalTime checkIn, LocalTime checkOut, double totalHours) {
+        this.id = id;
+        this.employeeId = employeeId;
+        this.date = date;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+        this.totalHours = totalHours;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getCheckIn() {
+        return checkIn;
+    }
+
+    public void setCheckIn(LocalTime checkIn) {
+        this.checkIn = checkIn;
+    }
+
+    public LocalTime getCheckOut() {
+        return checkOut;
+    }
+
+    public void setCheckOut(LocalTime checkOut) {
+        this.checkOut = checkOut;
+    }
+
+    public double getTotalHours() {
+        return totalHours;
+    }
+
+    public void setTotalHours(double totalHours) {
+        this.totalHours = totalHours;
+    }
 }
